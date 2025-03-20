e20304e08b813c056bc2fe9da253f9aaeedf41ec
Esma Avci <esmac@hotmail.fr>
2025-03-20 12:28:52 +0100

    ajout de php

--- afficherTexte.php
+++ afficherTexte.php
@@ -36,3 +36,31 @@ $recipes = [
     </ul>
 </body>
 </html>
+<?php
+
+// Déclaration du tableau des recettes
+$recipes = [
+    ['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
+    ['Couscous','[...]','mickael.andrieu@exemple.com',false,],
+];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Affichage des recettes</title>
+</head>
+<body>
+    <ul>
+        <?php for ($lines = 0; $lines <= 1; $lines++): ?>
+            <li><?php echo $recipes[$lines][0] . ' (' . $recipes[$lines][2] . ')'; ?></li>
+        <?php endfor; ?>
+    </ul>
+</body>
+</html>
+
+
+for ($lines = 0; $lines <= 2; $lines++) {
+    echo $users[$lines]['full_name'] . ' ' . $users[$lines]['email'] . '<br />';
+}
